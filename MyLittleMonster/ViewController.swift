cf0b9eb76e432438eceb9b1791be6b1d04fe8927
Карина <karina@macbook-air-karina.local>
2016-03-31 22:50:15 +0300

    Initial Commit

---
+++ MyLittleMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyLittleMonster
+//
+//  Created by Карина on 31.03.16.
+//  Copyright © 2016 Карина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
